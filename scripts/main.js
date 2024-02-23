09a69022afbb8496915f7fbf531227f6c434f8d1
yasunori <0528.taiki.sax@gmail.com>
2024-02-24 01:12:03 +0900

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
 function setUserName() {
     let myName = prompt("あなたの名前を入力してください。");
     if (!myName) {
         setUserName();
     } else {
-        localStorage.setItem("name", myName);
+        localStorage.setItem('name', myName);
         myHeading.textContent = `${myName} さん、Mozilla はかっこいいよ。`;
     }
 }
   
-if (!localStorage.getItem("name")) {
+if (!localStorage.getItem('name')) {
     setUserName();
 } else {
-    const storedName = localStorage.getItem("name");
+    const storedName = localStorage.getItem('name');
     myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
 }
 myButton.onclick = () => {
