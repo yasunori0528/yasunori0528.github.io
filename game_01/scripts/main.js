0c7c5add3f95a6b70a8458aaa92184bc8f29953a
yasunori <0528.taiki.sax@gmail.com>
2024-05-17 17:17:28 +0900

    a

--- game_01/scripts/main.js
+++ game_01/scripts/main.js
@@ -1,8 +1,9 @@
 window.onload = function(){
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    ctx.fillStyle = "green";
+    //ctx.fillStyle = "green";
     //ctx.fillRect(20, 10, 150, 100);
+    ctx.font = "50px serif";
     a = Math.random();
     ctx.fillText(toString(a), 0, 0, 140);
 }
