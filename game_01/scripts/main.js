cc63dab37d6b2de455973f7bd849b12b6abcd229
yasunori <0528.taiki.sax@gmail.com>
2024-05-17 17:10:10 +0900

    a

--- game_01/scripts/main.js
+++ game_01/scripts/main.js
@@ -1,4 +1,6 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-ctx.fillStyle = "green";
-ctx.fillRect(20, 10, 150, 100);
+window.onload = function(){
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(20, 10, 150, 100);
+}
