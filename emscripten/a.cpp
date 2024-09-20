aae91b673a3cc53105cfa62c4263f2baf8696ce2
yasunori0528 <0528.taiki.sax@gmail.com>
2024-09-20 13:40:02 +0900

    a

--- emscripten/a.cpp
+++ emscripten/a.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
-int main() {
-    int x;
-    std::cin >> x;
-    std::cout << x << std::endl;
-    return 0;
+// 足し算
+extern "C" {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // 引き算
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // 掛け算
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // 割り算
+    int divide(int a, int b) {
+        if (b == 0) {
+            return 0; // ゼロ除算防止
+        }
+        return a / b;
+    }
 }
